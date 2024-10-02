5279f469ddef8d34b6b6309b40f1f0cd2fa3d438
solrock-da-coder <gorb4758@gmail.com>
2024-10-02 12:30:05 -0400

    Create CinnabarIsland.asm
    adds grass data for cinnabar island

---
+++ data/wild/maps/CinnabarIsland.asm
@@ -0,0 +1,16 @@
+CinnabarIslandWildMons:
+	def_grass_wildmons 25 ; encounter rate
+	db  10, PIDGEY
+	db  9, PIDGEY
+	db  9, RATTATA
+	db  10, RATTATA
+	db  9, GROWLITHE
+	db  10, GROWLITH
+	db  10, VULPIX
+	db  9, VULPIX
+	db  10, KOFFING
+	db  15, MAGMAR
+	end_grass_wildmons
+
+	def_water_wildmons 0 ; encounter rate
+	end_water_wildmons
